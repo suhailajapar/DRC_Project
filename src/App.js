3aa06dafa1e7ee86540cb51145a5f0fb1922b73c
Suhaila Japar <suhaila@besquare.com.my>
2022-02-17 19:59:55 +0800

    Homepage & other features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import "./App.css";
 import Home from "./components/Home/Home";
+import Navbar from "./components/Home/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Home></Home>
-    </div>
+    <BrowserRouter>
+      <Navbar className="navbar" />
+      <Home className="home" />
+    </BrowserRouter>
   );
 }
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,53 +1,23 @@
 import React from "react";
-import { Navbar, Button, Nav } from "react-bootstrap";
+import Navbar from "./../Home/Navbar";
+import Footer from "./../Footer/Footer";
 import "./Home.css";
-import { ReactComponent as Logo } from "./../../assets/Hikers Logo Asset/Hikers big (Dark mode)-shrink.svg";
 
 function Home() {
   return (
     <>
-      <Navbar
-        className="py-1 px-3 row justify-content-between"
-        bg="dark"
-        variant="dark"
-      >
-        <Nav className="col align-items-center">
-          <a href="#home">
-            <Logo className="logo-size" />
-          </a>
-          <Navbar.Brand className="pb-2 nav-title" href="#home">
-            Hikers
-          </Navbar.Brand>
-          <Nav.Link
-            className="px-0 text-white nav-text nav-subtitle"
-            href="#home"
-          >
-            Market
-          </Nav.Link>
-        </Nav>
-
-        <Nav className=" col justify-content-end">
-          <Nav.Link
-            className="text-white nav-text nav-subtitle"
-            href="#features"
-          >
-            Login
-          </Nav.Link>
-          <div className="divider"></div>
-          <Button
-            variant="success"
-            className="h-50 mt-1 p-1 nav-text nav-subtitle"
-            href="#pricing"
-          >
-            Sign Up
-          </Button>
-        </Nav>
-      </Navbar>
-
+      <Navbar />
       <div className="landingPage">
         <section className="lpage" id="four">
-          <div>
-            <h1>Hello</h1>
+          <div className="fourth">
+            <div className="fourth-title">
+              CHOOSE HIKERS
+              <br />
+              GET RICHER
+            </div>
+            <div className="lpage-btn">
+              <a href="/">Register Now!</a>
+            </div>
           </div>
         </section>
         <section className="lpage" id="three">
@@ -60,10 +30,19 @@ function Home() {
             <h1>Hello</h1>
           </div>
         </section>
-        <section className="lpage" id="one">
-          <div>
-            <h1>Hello</h1>
+        <section className="lpage lpage-one" id="one">
+          <div className="first">
+            <div className="first-title">Trade with Us</div>
+            <div className="first-subtitle">
+              Every successful trader
+              <br />
+              has to start somewhere
+            </div>
+            <div className="lpage-btn first-btn">
+              <a href="/">Start Trading Now!</a>
+            </div>
           </div>
+          <Footer className="home-footer" />
         </section>
       </div>
     </>
