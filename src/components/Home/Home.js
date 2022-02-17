6109660a21c6da7f60b8d012305d49c15d4c19a1
Darvind Chandrasekaran <darvind.chandra@gmail.com>
2022-02-17 09:28:07 +0800

    updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,51 +1,52 @@
 import React from "react";
 import { Navbar, Button, Nav } from "react-bootstrap";
 import "./Home.css";
-import "./../Market/Market";
+import Marketpage from "./../Market/Market";
 import { ReactComponent as Logo } from "./../../assets/Hikers Logo Asset/Hikers big (Dark mode)-shrink.svg";
 
 function Home() {
   return (
     <>
-      <Navbar
-        className="py-1 px-3 row justify-content-between"
-        bg="dark"
-        variant="dark"
-      >
-        <Nav className="col align-items-center">
-          <a href="#home">
-            <Logo className="logo-size" />
-          </a>
-          <Navbar.Brand className="pb-2 nav-title" href="#home">
-            Hikers
-          </Navbar.Brand>
-          <Nav.Link
-            className="px-0 text-white nav-text nav-subtitle"
-            href="#home"
-          >
-            Market
-          </Nav.Link>
-        </Nav>
-
-        <Nav className=" col justify-content-end">
-          <Nav.Link
-            className="text-white nav-text nav-subtitle"
-            href="#features"
-          >
-            Login
-          </Nav.Link>
-          <div className="divider"></div>
-          <Button
-            variant="success"
-            className="h-50 mt-1 p-1 nav-text nav-subtitle"
-            href="#pricing"
-          >
-            Sign Up
-          </Button>
-        </Nav>
-      </Navbar>
-
       <div className="landingPage">
+        <Navbar
+          className="py-1 px-3 row justify-content-between"
+          id="nav-barFlow"
+          bg="dark"
+          variant="dark"
+        >
+          <Nav className="col align-items-center">
+            <a href="#home">
+              <Logo className="logo-size" />
+            </a>
+            <Navbar.Brand className="pb-2 nav-title" href="#home">
+              Hikers
+            </Navbar.Brand>
+            <Nav.Link
+              className="px-0 text-white nav-text nav-subtitle"
+              href="/Marketpage"
+            >
+              Market
+            </Nav.Link>
+          </Nav>
+
+          <Nav className=" col justify-content-end">
+            <Nav.Link
+              className="text-white nav-text nav-subtitle"
+              href="#features"
+            >
+              Login
+            </Nav.Link>
+            <div className="divider"></div>
+            <Button
+              variant="success"
+              className="h-50 mt-1 p-1 nav-text nav-subtitle"
+              href="#pricing"
+            >
+              Sign Up
+            </Button>
+          </Nav>
+        </Navbar>
+
         <section className="lpage" id="four">
           <div>
             <h1>Hello</h1>
