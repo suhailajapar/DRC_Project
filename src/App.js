c1ff8558ed07b90fa533a550d762b997e2113ac0
Safra Adlina <adlina.ssr@gmail.com>
2022-02-18 09:24:44 +0800

    sign-up pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Home from "./components/Home/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Signup from "./components/Signup/Signup";
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      {/* <Home /> */}
+      <Signup />
     </div>
   );
 }
