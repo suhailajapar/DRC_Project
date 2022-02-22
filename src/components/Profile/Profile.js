b4b7cc248a1d4a766a0487a455db2e641a2fc5ec
aminsyahmii <aminsyahmi11@gmail.com>
2022-02-22 14:16:52 +0800

    add fx

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,49 +1,91 @@
 import React from "react";
 // import { Navbar, Container, Nav } from "react-bootstrap";
 import "./Profile.css";
+import ReactDOM from "react-dom";
+import { useForm } from "react-hook-form";
 
 function Profile() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+  }; // your form submit function which will invoke after successful validation
+
   return (
     <div className="main-wrapper">
-      {/* <Navbar className="nav-bar" bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Hikers</Navbar.Brand>
-          <Nav className="nav-name">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar> */}
       <div className="photo-box"></div>
       <div className="container1">
         {" "}
         <div className="username">Username: </div>
         <div className="date-joined">Date-Joined : </div>
       </div>
-      <div>
-        <div className="div1">
-          <h1 className="name">Name: </h1>
-          <input className="name-input"></input>
-        </div>
-        <div className="div1">
-          <h2 className="mobile">Mobile number :</h2>
-          <input className="mobile-input"></input>
-        </div>
-        <div className="div1">
-          <h3 className="email">Email :</h3>
-          <input className="email-input" type="text" />
-        </div>
-        <div className="div1">
-          <h4 className="pw">Password :</h4>
-          <input className="pw-input"></input>
-        </div>
-      </div>
-      <div>
-        <button className="Save-Button">Save</button>
+      <div className="div1">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <h1 className="headers">
+            Full Name :
+            <input
+              className="InputBox"
+              {...register("fullname", {
+                required: true,
+                maxLength: 50,
+                pattern: /^[a-zA-Z ]*$/,
+              })}
+            />
+          </h1>
+          {errors?.fullname?.type === "required" && (
+            <p>This field is required</p>
+          )}
+          {errors?.fullname?.type === "maxLength" && (
+            <p>Full name cannot exceed 50 characters</p>
+          )}
+          {errors?.fullname?.type === "pattern" && (
+            <p>Alphabetical characters only</p>
+          )}
+          <h1 className="headers">
+            Email :
+            <input
+              className="InputBox"
+              {...register("Email", {
+                pattern: /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/,
+                required: true,
+              })}
+            />
+            {errors?.Email?.type === "pattern" && <p>Enter valid email only</p>}
+            {errors?.Email?.type === "required" && (
+              <p>This field is required</p>
+            )}
+          </h1>
+          <h1 className="headers">
+            Mobile Number :
+            <input
+              className="InputBox"
+              {...register("MobileNumber", {
+                required: true,
+                minlegth: 10,
+                maxlength: 15,
+                pattern: /\d+/,
+              })}
+            />
+          </h1>
+          {errors?.MobileNumber?.type === "pattern" && (
+            <p>Valid Mobile Number only</p>
+          )}
+          {errors.MobileNumber && <p>Min 10 digits</p>}
+          {errors?.MobileNumber?.type === "required" && (
+            <p>This field is required</p>
+          )}
+          <button className="Save-Button">Save </button>
+        </form>
       </div>
     </div>
   );
 }
 
 export default Profile;
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Profile />, rootElement);
